5913b0bbf3afe371797c0f6937a161407964f5ba
nerea <nereagomez@workoholics.es>
2025-03-28 13:34:10 +0100

    asjutes suavizado scroll & transiciones

--- lara-gonzalez-web/src/components/wrapper/ImageWrapper.tsx
+++ lara-gonzalez-web/src/components/wrapper/ImageWrapper.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const ImageWrapper = ({ image }: { image: any }) => {
     return (
-      <div className="image-wrapper">
-        <img src={image.url} alt={image.alt} width={image.width} height={image.height} />
-      </div>
+      <picture className="image-wrapper">
+        <img src={image.url} alt={image.alt} width={image.width} height={image.height} loading="lazy"/>
+      </picture>
     );
   };
   
