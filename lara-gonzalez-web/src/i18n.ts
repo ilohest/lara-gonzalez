94205d9e4013c2724f759f6953e76ab1525bd427
nerea <nereagomez@workoholics.es>
2025-07-09 11:38:53 +0200

    update

--- lara-gonzalez-web/src/i18n.ts
+++ lara-gonzalez-web/src/i18n.ts
@@ -2,8 +2,8 @@ export const defaultLocale = 'es';
 
 export const locales = {
   es: 'Español',
-  en: 'English', 
-  fr: 'Français', 
+  // en: 'English', 
+  // fr: 'Français', 
 };
 
-export const languages = Object.keys(locales); // ['es', 'en', 'fr']
+export const languages = Object.keys(locales);
